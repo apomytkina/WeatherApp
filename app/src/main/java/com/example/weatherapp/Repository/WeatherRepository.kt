7de89e1b5a2cda28830ed91676335d6bebf1302f
apomytkina <annapoma90@mail,ru>
2021-09-14 12:16:18 +0300

    UI package added

--- app/src/main/java/com/example/weatherapp/Repository/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherapp/Repository/WeatherRepository.kt
@@ -1,12 +1,11 @@
 package com.example.weatherapp.Repository
 
-import androidx.lifecycle.LiveData
 import com.example.weatherapp.Api.WeatherApi
 import javax.inject.Inject
 
 class WeatherRepository @Inject constructor(
     private val weatherApi: WeatherApi
 ) {
-    suspend fun getCurrentWeather(cityName:String) = weatherApi.getCurrentWeather(city = cityName)
-    suspend fun getForecast(cityName: String) = weatherApi.getForecast(city = cityName)
+    suspend fun getCurrentWeather(cityName:String) = weatherApi.getCurrentWeather(cityName)
+    suspend fun getForecast(cityName: String) = weatherApi.getForecast(cityName)
 }
--- app/src/main/java/com/example/weatherapp/UI/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/UI/MainActivity.kt
@@ -1,16 +1,16 @@
-package com.example.weatherapp
+package com.example.weatherapp.UI
 
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.example.weatherapp.R
 import com.example.weatherapp.Repository.WeatherRepository
 import com.example.weatherapp.ViewModel.WeatherViewModel
+import com.example.weatherapp.WeatherViewModelFactory
 import com.example.weatherapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Job
